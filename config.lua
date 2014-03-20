12b37c418c0e2c6e63a9582207e37ef61b353730
Leonardo <leonardo.lobato.77@gmail.com>
2014-03-19 23:12:43 -0300

    Guerra dos cubos atualizado

---
+++ config.lua
@@ -0,0 +1,31 @@
+application = {
+	content = {
+        graphicsCompatibility = 1,
+		width = 320,
+		height = 480,
+        scale = "zoomEven", 
+		--scale = "letterBox",
+		fps = 30,
+		
+		--[[
+        imageSuffix = {
+		    ["@2x"] = 2,
+		}
+		--]]
+	},
+
+    --[[
+    -- Push notifications
+
+    notification =
+    {
+        iphone =
+        {
+            types =
+            {
+                "badge", "sound", "alert", "newsstand"
+            }
+        }
+    }
+    --]]    
+}
